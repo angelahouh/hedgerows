93e4ef2c9ab267236dea769b281d8a02bce0654e
angelahouh <ahouh@thetrustees.org>
2025-02-13 10:47:16 -0500

    powisset microbiometer analysis graphs

--- powisset_microbiometer_script.R
+++ powisset_microbiometer_script.R
@@ -1,33 +1,54 @@
 library(tidyverse)
-setwd("C:/Users/ahouh/OneDrive - TTOR/Documents/hedgerows")
-powmicro <- read_csv("powisset_hedgerows_microbiom2024.csv")
-
-powmicro |>
-  select(hedgerow, sample_depth_cm, distance_from_hedgerow_ft, microbes_ugC_per_gsoil, FB_ratio, fungal_percent, bacterial_percent)
-
-#this is a box plot that shows how microbe density changes with distance from the hedgerow
-ggplot(powmicro, aes(x = factor(distance_from_hedgerow_ft), y = microbes_ugC_per_gsoil))+
+library(readr)
+setwd("C:/Users/ahouh/OneDrive/hedgerows")
+powmicro <- read_csv("powisset_hedgerows_microbiometer.csv")
+#####################################################################
+#analyzing how microbial density changes with distance from hedgerow
+ggplot(powmicro, aes(x = factor(Distance_from_hedgerow_feet), y = microbes_ugC_per_gsoil))+
   geom_boxplot() +
   labs(
     y = "Microbes (ugC/g soil)", x = "Distance from Hedgerow (ft)",
-    title = "How Microbe Density Changes with Distance from Hedgerow"
+    title = "Microbe Density with Distance from Hedgerow"
   )
-
-#edit graph titles
-ggplot(powmicro, aes(x = factor(distance_from_hedgerow_ft), y = microbes_ugC_per_gsoil))+
-  geom_point(aes(color = sample_depth_cm)) +
-  facet_wrap(~hedgerow) +
+#vs
+ggplot(powmicro, aes(x = factor(Distance_from_hedgerow_feet), y = microbes_ugC_per_gsoil))+
+  geom_boxplot() +
+  facet_wrap(~sampling_layer) +
   labs(
     y = "Microbes (ugC/g soil)", x = "Distance from Hedgerow (ft)",
-    title = "How Microbe Density Changes with Distance from Each Hedgerow Type"
+    title = "Microbe Density with Distance from Hedgerow, by Depth"
   )
-
-#this box plot shows how microbe density changes with sample depth for each hedgerow
-ggplot(powmicro, aes(x = factor(sample_depth_cm), y = microbes_ugC_per_gsoil)) +
+#vs
+ggplot(powmicro, aes(x = factor(Distance_from_hedgerow_feet), y = microbes_ugC_per_gsoil))+
+  geom_point(aes(color = sampling_layer)) +
+  facet_wrap(~Hedgerow) +
+  labs(
+    y = "Microbes (ugC/g soil)", x = "Distance from Hedgerow (ft)",
+    title = "Microbe Density with Distance from Hedgerow, by Type"
+  )
+####################################################################
+#analyzing relationship between microbe density and sample depth
+ggplot(powmicro, aes(x = factor(sampling_layer), y = microbes_ugC_per_gsoil)) +
   geom_boxplot() +
   labs(
     y = "Microbes (ugC/g soil)", x = "Sample Depth (cm)",
-    title = "How Microbe Density Changes with Soil Depth"
+    title = "How Microbe Density Changes with Soil Depth, by Type"
   ) +
-  facet_wrap(~hedgerow)
+  facet_wrap(~Hedgerow)
+####################################################################
+#analyzing relationship between sample depth and fungal/bacterial %
+ggplot(powmicro, aes(x = factor(sampling_layer), y = Fungal_percent)) +
+  geom_boxplot()+
+  facet_wrap(~Hedgerow)+
+  labs(
+    y = "Fungal Percent", x = "Sample Depth"
+  )
 
+ggplot(powmicro, aes(x = factor(sampling_layer), y = Bacterial_percent)) +
+  geom_boxplot()+
+  facet_wrap(~Hedgerow)+
+  labs(
+    y = "Bacterial Percent", x = "Sample Depth"
+  )
+
+  )
